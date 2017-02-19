0a4629ef2e0eab3760f0ea2e987f543da3f05e64
Benchan23 <bednayb@gmail.com>
2017-02-19 11:22:25 +0100

    add hideAndShow.js for hide and show

---
+++ docs/hideAndShow.js
@@ -0,0 +1,10 @@
+// HIDE the TEXT
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+// SHOW the TEXT
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
